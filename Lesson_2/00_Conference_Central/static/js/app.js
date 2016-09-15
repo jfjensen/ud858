54c0f55f73430ba4b27b224f1e0beeedfbcee6fa
Jes Fink-Jensen <jes@finkjensen.be>
2016-09-15 11:54:03 +0200

    Finished Lesson 2

--- Lesson_2/00_Conference_Central/static/js/app.js
+++ Lesson_2/00_Conference_Central/static/js/app.js
@@ -86,7 +86,7 @@ app.constant('HTTP_ERRORS', {
  */
 app.factory('oauth2Provider', function ($modal) {
     var oauth2Provider = {
-        CLIENT_ID: 'web-client-id',
+        CLIENT_ID: '972376087521-3efsmutdst4ict1v3nedoj2e5pcga365.apps.googleusercontent.com',
         SCOPES: 'email profile',
         signedIn: false
     }
